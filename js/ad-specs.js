a581da21e53c60b3907cb1267c6153d84e0c29eb
Dustin Pearce <Dustin.Pearce@babycenter.com>
2013-04-14 22:24:18 -0700

    Added international ad-specs and excel downloads

---
+++ js/ad-specs.js
@@ -0,0 +1,25 @@
+var intl_ad_specs = {
+    wallpaper: {
+        size: '1280w with 1100w white space centered in the image',
+        fileTypes: 'GIF or JPG plus click-through URL;        Logo must be smaller than BabyCenter logo and 250 pixels down from the top of the page; BabyCenter does not allow the color black or red as a background color; Any talent or product packaging imagery must be below the top 250 pixels of the page and cannot be larger than 125 pixels high and 60 pixels wide; Any direct CTA included in the design must appear in the right rail only and cannot be included in the left rail; Creative is subject to BabyCenter approval and must be submitted 10 business days prior to launch.',
+
+    },
+    filmStrip: {
+        size: '300w x 600h',
+        maxFileSize: 'Initial 40KB, Secondary 140KB',
+        characterMax: 'N/A',
+        loopTime: '15 seconds total animation; no more than 3 loops',
+        fileTypes: '3rd party tag',
+        thirdParty: 'BabyCenter web sites support the following 3rd party tags: Atlas, DFA*, DoubleClick Studio, Flashtalking, MediaMind, PointRoll. All other vendors must be tested and approved on a case-by-case basis. <br/> * DFA internal redirects in .txt file       See Rich Media Guidelines'
+    },
+    billboard : {
+        size: '•Collapsed: 62w x 88h or 196w x 31h <br/> •Expanded: 970w x 250h',
+        maxFileSize: 'Initial 40KB, Secondary 140KB',
+        characterMax: 'N/A',
+        loopTime: '15 seconds total animation; no more than 3 loops',
+        fileTypes: '3rd party tag',
+        thirdParty: 'BabyCenter web sites support the following 3rd party tags: Atlas, DFA*, DoubleClick Studio, Flashtalking, MediaMind, PointRoll. All other vendors must be tested and approved on a case-by-case basis. <br/> * DFA internal redirects in .txt file       See Rich Media Guidelines'
+    }
+}
+
+
